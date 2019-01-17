eb3454c8b6c66a803fbd27238c38be1d2d5a9dae
Keitaro Oguri <25946200+ogukei@users.noreply.github.com>
2019-01-18 02:21:31 +0900

    Add light shader

--- main.c
+++ main.c
@@ -8,87 +8,81 @@
 #include <CoreVideo/CoreVideo.h>
 #include <OpenGL/CGLCurrent.h>
 #include <OpenGL/gl3.h>
-
 #include "private.h"
 
+// Vertex Shader
 static const char *kVertexShader =
 "#version 400\n"
-"in vec3 vp;"
+"in vec3 position;"
 "void main() {"
-"  gl_Position = vec4(vp, 1.0);"
+"    gl_Position = vec4(position, 1.0);"
 "}";
 
+// Fragment Shader
 static const char *kFragmentShader =
 "#version 400\n"
 "out vec4 color;"
 "uniform vec4 bounds;"
-"uniform float time;"
+"uniform float time;\n"
 "void main() {"
-"  vec2 v = gl_FragCoord.xy / bounds.zw;"
-"  vec2 c = vec2(0.5, 0.5);"
-"  float p = (cos(time * 2.0) + 1.0) * 0.5;"
-"  float d = distance(v, c) * 2.0;"
-"  float a = 1.0 - clamp(abs(p - d), 0, 1);"
-"  color = vec4(a, a, a, a);"
+"    vec2 v = gl_FragCoord.xy / bounds.zw;"
+"    vec2 p = vec2(sin(time) * 0.1 + 0.5, sin(time + 1) * 0.1 + 0.5);"
+"    float d = distance(v, p) * 2.0;"
+"    float r = cos(time) * 0.05 + 0.075;"
+"    vec3 k = vec3(1.0, 2.0 / r, 1.0 / (r * r));"
+"    float a = 1.0 / (k.x + k.y*d + k.z*d*d);"
+"    color = vec4(a, a, a, 0.9);"
 "}";
 
 typedef struct {
     GLuint bounds;
     GLuint time;
-}
-ShaderParameters;
+} ShaderParameters;
 
 typedef struct {
-    CGSConnectionID connection;
-    CGSWindowID window;
     CGRect bounds;
     CGLContextObj context;
     ShaderParameters shader;
-}
-Environment;
+} Environment;
 
 static bool MakeGL(const char *vertex_shader, const char *fragment_shader, GLuint *program);
 static ShaderParameters MakeShaderParameters(GLuint program);
 static bool VerifyShader(GLuint shader, const char *label);
-static bool MakeCGLContext(CGSConnectionID, CGSWindowID, CGRect, CGLContextObj *);
+static bool MakeCGL(CGSConnectionID, CGSWindowID, CGRect, CGLContextObj *);
 static bool MakeWindow(CGSConnectionID, CGRect, CGSWindowID *);
 static CVReturn RenderFrame(CVDisplayLinkRef, const CVTimeStamp *, const CVTimeStamp *, 
     CVOptionFlags, CVOptionFlags *, void *);
 
 int main(void) {
     CGSConnectionID connection = CGSMainConnectionID();
-    CGRect frame = CGRectMake(0, 0, 600, 400);
-    CGRect display_bounds;
-    if (CGSGetDisplayBounds(CGSMainDisplayID(), &display_bounds) == kCGSErrorSuccess) {
-        CGFloat x = CGRectGetMidX(display_bounds) - CGRectGetWidth(frame) * 0.5;
-        CGFloat y = CGRectGetMidY(display_bounds) - CGRectGetHeight(frame) * 0.5;
-        frame = CGRectMake(x, y, CGRectGetWidth(frame), CGRectGetHeight(frame));
-    }
+    CGRect frame = CGRectMake(0, 0, 400, 400);
+    // creates a window
     CGSWindowID window = 0;
-    MakeWindow(connection, frame, &window);
-
+    bool ok = MakeWindow(connection, frame, &window);
+    assert(ok);
+    // adds an OpenGL surface onto the window
     CGRect bounds = CGRectMake(0, 0, CGRectGetWidth(frame), CGRectGetHeight(frame));
     CGLContextObj cgl_context;
-    MakeCGLContext(connection, window, bounds, &cgl_context);
+    ok = MakeCGL(connection, window, bounds, &cgl_context);
+    assert(ok);
     CGLSetCurrentContext(cgl_context);
+    // compiles GLSL shader
     GLuint program;
-    MakeGL(kVertexShader, kFragmentShader, &program);
+    ok = MakeGL(kVertexShader, kFragmentShader, &program);
+    assert(ok);
     ShaderParameters shader = MakeShaderParameters(program);
     CGLSetCurrentContext(NULL);
-
     Environment *environment = &(Environment) {
-        .connection = connection,
-        .window = window,
         .bounds = bounds,
         .context = cgl_context,
         .shader = shader
     };
-
-    CVDisplayLinkRef display;
+    // starts frame rendering with Display Link
+    CVDisplayLinkRef display = NULL;
     CVDisplayLinkCreateWithCGDisplay(CGSMainDisplayID(), &display);
     CVDisplayLinkSetOutputCallback(display, &RenderFrame, environment);
     CVDisplayLinkStart(display);
-    sleep(1000);
+    sleep(10);
     CVDisplayLinkStop(display);
     return 0;
 }
@@ -103,13 +97,11 @@ static CVReturn RenderFrame(CVDisplayLinkRef display,
     double time = time_output->hostTime / (double)CVGetHostClockFrequency();
     CGLLockContext(context);
     CGLSetCurrentContext(context);
-
     glClear(GL_COLOR_BUFFER_BIT);
     glUniform4f(shader->bounds, 0, 0, CGRectGetWidth(bounds), CGRectGetHeight(bounds));
     glUniform1f(shader->time, (float)time);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     CGLFlushDrawable(context);
-
     CGLSetCurrentContext(NULL);
     CGLUnlockContext(context);
     return 0;
@@ -160,7 +152,6 @@ static bool MakeGL(const char *vertex_shader, const char *fragment_shader, GLuin
     // Settings
     glEnable(GL_BLEND);
     glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
-    // Done
     glUseProgram(program);
     glBindVertexArray(vao);
     *program_ref = program;
@@ -220,7 +211,7 @@ static bool MakeWindow(CGSConnectionID connection, CGRect frame, CGSWindowID *re
     return true;
 }
 
-static bool MakeCGLContext(CGSConnectionID connection, CGSWindowID window, CGRect bounds, CGLContextObj *ref) {
+static bool MakeCGL(CGSConnectionID connection, CGSWindowID window, CGRect bounds, CGLContextObj *ref) {
     CGLPixelFormatAttribute attributes[] = {
         kCGLPFAOpenGLProfile, (CGLPixelFormatAttribute)kCGLOGLPVersion_3_2_Core,
         kCGLPFAColorSize, 24,
